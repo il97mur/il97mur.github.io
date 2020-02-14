90ba04a6ac12cb0cbea13f5d5cc3286ca0d13e23
il97mur <ilha9749@gmail.com>
2020-02-15 00:25:04 +0600

    First commit

---
+++ Picnmix/feedback.php
@@ -0,0 +1,53 @@
+<?php
+
+
+    if ($_POST['e-mail'] == true) {
+    $mail = $_POST['e-mail'];
+    if ($_POST['name'] == true) {
+        $name = $_POST['name'];
+    } else {
+        $name = 'Отправитель';
+    };
+
+    if ($_POST['telephone'] == true) {
+        $phone = $_POST['telephone'];
+    } else {
+        $phone = 'Телефон';
+    };
+
+    if ($_POST['comment'] == true) {
+        $comment = $_POST['comment'];
+    } else {
+        $comment = 'Сообщение отсутствует.';
+    };
+
+    $mess = '
+    Отправитель: ' . $name . '
+    E-mail: ' . $mail . '
+    Телефон: ' . $phone . '
+    Сообщение: ' . $comment;
+
+    $email = 'picnmix@mail.com';
+    $headers = "From: $email\r\nReply-To: $email" . "\r\n" . "MIME-Version: 1.0\r\nContent-type: text/plain; charset=utf-8";
+
+    mail('alik@picnmix.ru', 'Форма обратной связи', $mess, $headers);
+    header("Refresh:5; index.html");
+    echo ('<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body><h1>Ваше сообщение успешно отправлено!</h1>
+    </body>');
+    $title = 'Сообщение отправлено';
+} else {
+    header("Refresh:5; index.html");
+
+    echo ('<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body><h1 class="red">Ошибка отправки сообщения!</h1><p>Проверьте правильность заполненных данных.<br>
+    Через 5 секунд вы вернетесь на главную страницу.');
+    $title = 'Ошибка!';
+
+};
+
+echo ('<br/><br/>');
+
+?>
